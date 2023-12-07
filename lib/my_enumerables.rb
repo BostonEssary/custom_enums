0bcd7b22fbc71a4b556664701bb35909300b9911
Boston Essary <BostonCEssary@gmail.com>
2023-12-07 16:36:25 -0700

    first commit

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,27 @@
 module Enumerable
   # Your code goes here
+
+  def my_each_with_index
+    index = 0
+    if block_given?
+      while index <= self.length - 1
+        yield self[index], index
+        index = index + 1
+      end
+    end
+    return self
+  end
+
+  def my_select
+    if block_given?
+      arr = []
+      self.my_each do |value|
+
+        yield value
+      end
+    end
+    return [] 
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +30,15 @@ end
 # to this method
 class Array
   # Define my_each here
+
+  def my_each
+      index = 0
+      if block_given?
+        while index <= self.length - 1
+          yield self[index]
+          index = index + 1
+        end
+      end
+      return self
+  end 
 end
